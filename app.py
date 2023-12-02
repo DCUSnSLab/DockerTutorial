30d368ea44c85631939ab43192dc5ee9d6fa12e4
soobinJeon <marsberry@cu.ac.kr>
2023-12-02 15:42:05 +0900

    test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask inside Docker!! Please check it!!!!! what is this!?"
+    return "Flask inside Docker!! Please check it!!!!! what is this!? ffff"
 
 
 if __name__ == "__main__":

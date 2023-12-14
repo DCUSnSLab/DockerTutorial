55de02aa150af3807571b2e5d7d87b670ecd2ced
soobin Jeon <marsberry@cu.ac.kr>
2023-12-14 16:27:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "app test latest latest latest web hook test hello soobin 2"
+    return "app test latest latest latest web hook test hello soobin 2!!!!!!!!!!!!!"
 
 
 if __name__ == "__main__":
